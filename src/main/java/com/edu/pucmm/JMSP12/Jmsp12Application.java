d2834e0f8022d3d3762459af5d02a4fff29989c1
Dany <ridoreda1992@gmail.com>
2017-10-25 00:53:15 -0400

    Controller la

--- src/main/java/com/edu/pucmm/JMSP12/Jmsp12Application.java
+++ src/main/java/com/edu/pucmm/JMSP12/Jmsp12Application.java
@@ -21,38 +21,39 @@ import java.util.Date;
 
 @EnableJms
 @SpringBootApplication
-public class Jmsp12Application implements ApplicationRunner {
+//implements ApplicationRunner
+public class Jmsp12Application  {
     //private static final Logger logger = LoggerFactory.getLogger(Application.class);
-	@Bean
-	public JmsListenerContainerFactory<?> temperaturaFactory (ConnectionFactory connectionFactory, DefaultJmsListenerContainerFactoryConfigurer configurer){
-		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-		configurer.configure(factory, connectionFactory);
-		factory.setErrorHandler(
-				t -> System.err.println("An error has occurred in the transaction"));
-		// lambda function
-		factory.setErrorHandler(t -> System.err.println("An error has occurred in the transaction"));
-		configurer.configure(factory, connectionFactory);
-		return factory;
-	}
+//	@Bean
+//	public JmsListenerContainerFactory<?> temperaturaFactory (ConnectionFactory connectionFactory, DefaultJmsListenerContainerFactoryConfigurer configurer){
+//		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+//		configurer.configure(factory, connectionFactory);
+//		factory.setErrorHandler(
+//				t -> System.err.println("An error has occurred in the transaction"));
+//		// lambda function
+//		factory.setErrorHandler(t -> System.err.println("An error has occurred in the transaction"));
+//		configurer.configure(factory, connectionFactory);
+//		return factory;
+//	}
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(Jmsp12Application.class, args);
-        JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
-        System.out.println("->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Sending a new Teperature");
-        jmsTemplate.convertAndSend("temperaturaMessage", new Temperatura(new Date(), 1, 16, 27));
+//        JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+//        System.out.println("->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Sending a new Teperature");
+//        jmsTemplate.convertAndSend("temperaturaMessage", new Temperatura(new Date(), 1, 16, 27));
 	}
-	@Bean
-	public MessageConverter jacksonJmsMessageConverter(){
-		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-		converter.setTargetType(MessageType.TEXT);
-		converter.setTypeIdPropertyName("_type");
-		return converter;
-	}
+//	@Bean
+//	public MessageConverter jacksonJmsMessageConverter(){
+//		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+//		converter.setTargetType(MessageType.TEXT);
+//		converter.setTypeIdPropertyName("_type");
+//		return converter;
+//	}
 
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        for (String name : args.getOptionNames()){
-            System.out.println(name);
-        }
-
-    }
+//    @Override
+//    public void run(ApplicationArguments args) throws Exception {
+//        for (String name : args.getOptionNames()){
+//            System.out.println(name);
+//        }
+//
+//    }
 }
