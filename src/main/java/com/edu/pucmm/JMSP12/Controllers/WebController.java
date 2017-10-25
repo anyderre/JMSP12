a5faab079b37b81d5f7949cfe1b66c54cc851ede
Dany <ridoreda1992@gmail.com>
2017-10-25 00:34:44 -0400

    Sa se template yo

--- src/main/java/com/edu/pucmm/JMSP12/Controllers/WebController.java
+++ src/main/java/com/edu/pucmm/JMSP12/Controllers/WebController.java
@@ -3,9 +3,9 @@ package com.edu.pucmm.JMSP12.Controllers;
 import com.edu.pucmm.JMSP12.Entity.JmsClient;
 import com.edu.pucmm.JMSP12.Entity.Temperatura;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 /**
  * Created by Dany on 24/10/2017.
@@ -15,8 +15,9 @@ public class WebController {
     @Autowired
     JmsClient jmsClient;
 
-    @RequestMapping(value="/produce", consumes = "application/json")
-    public String produce(@RequestParam("msg")Temperatura msg){
+    @RequestMapping(value="/guardar/tempetaruta", method = RequestMethod.POST, consumes = "application/json")
+    public String produce(@RequestBody Temperatura msg){
+        msg.setFecha(new Date());
         jmsClient.send(msg);
         return "Done";
     }
